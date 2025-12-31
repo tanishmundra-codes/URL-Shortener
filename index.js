43e5c9bc2704913d32f9c2af720e3e2d94bdb3c7
tanishmundra-codes <tanishmundra2@gmail.com>
2025-12-31 12:14:38 +0530

    Add login route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { connectDB } = require("./connection");
 const urlRoute = require("./routes/url")
 const cors = require("cors");
 const {handleRedirectUrl} = require("./controllers/url")
+const userRoute = require("./routes/user")
 
 const app = express();
 const PORT = 3000;
@@ -12,6 +13,7 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use("/url", urlRoute);
+app.use("/user", userRoute);
 
 app.get("/:shortID", handleRedirectUrl);
 
