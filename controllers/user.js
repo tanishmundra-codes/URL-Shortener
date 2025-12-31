abef29347a850dd592de029ac0e028fd6ef5e15b
tanishmundra-codes <tanishmundra2@gmail.com>
2025-12-31 12:15:25 +0530

    signup/login

---
+++ controllers/user.js
@@ -0,0 +1,46 @@
+const userModel = require("../models/user")
+
+async function handleSignup(req, res) {
+    const body = req.body;
+
+    try {
+        await userModel.createUser(
+            body.name,
+            body.email,
+            body.password
+        )
+
+        return res.status(201).json({status : "User created"})
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: "Database Error" });
+    }
+}
+
+async function handleLogin(req, res) {
+    const {email, password} = req.body;
+
+    try {
+        const user = await userModel.getUserByEmail(email)
+
+        if(!user) {
+            return res.status(401).json({error: "Invalid email or password"});
+        }
+
+        if (user.password_hash !== password) {
+            return res.status(401).json({ error: "Invalid email or password" });
+        }
+
+        return res.json({ message: "Login successful", user: user });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: "Database Error" });
+    }
+}
+
+
+
+module.exports= {
+    handleSignup,
+    handleLogin
+}
