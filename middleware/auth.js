d036c8559f0f2b8b7a95abacb54cdf304ee5499c
tanishmundra-codes <tanishmundra2@gmail.com>
2026-01-01 21:16:28 +0530

    user specific data

---
+++ middleware/auth.js
@@ -0,0 +1,28 @@
+const {getUser} = require("../util/auth")
+
+async function checkForLoginUser(req, res, next) {
+    const userUid = req.cookies?.uid
+
+    if(!userUid) {
+        return res.status(401).json({ error: "User not authenticated" });
+    }
+    const user = getUser(userUid);
+
+    if(!user){
+        return  res.status(401).json({ error: "User not authenticated" });
+    }
+
+    req.user = user;
+    next();
+}
+
+async function checkAuth(params) {
+    const userUid = req.cookies?.uid
+
+    const user = getUser(userUid);
+
+    req.user = user;
+    next();
+}
+
+module.exports = {checkForLoginUser,checkAuth};
