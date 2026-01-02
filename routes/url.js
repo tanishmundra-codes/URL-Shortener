5cf7f1457b043c9191d2f2e702ada2fe7f4863a5
tanishmundra-codes <tanishmundra2@gmail.com>
2026-01-02 11:52:59 +0530

    completed user specific

--- routes/url.js
+++ routes/url.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const {checkAuth} = require("../middleware/auth")
+
 const { handleCreateNewUrl,handleAnalytics, handleGetUserUrls } = require("../controllers/url")
 
 router.post("/", handleCreateNewUrl);
 router.get("/analytics/:id", handleAnalytics);
-router.get("/analytics/", handleGetUserUrls)
+router.get("/analytics/", checkAuth, handleGetUserUrls)
 
 module.exports = router;
