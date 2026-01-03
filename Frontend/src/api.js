73b5ed88ca73d433c21980acab5b2b02f2b1772e
tanishmundra-codes <tanishmundra2@gmail.com>
2026-01-03 12:34:31 +0530

    backendurl

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,3 +1,3 @@
-const backendUrl = import.meta.env.backendUrl || "http://localhost:3000";
+const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
 
 export default backendUrl;
