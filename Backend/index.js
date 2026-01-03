e5dec967415e7d2bfaa1936be0e34983a1da7591
tanishmundra-codes <tanishmundra2@gmail.com>
2026-01-03 12:18:20 +0530

    changes made

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,7 @@ connectDB();
 app.use(cors({
     origin: [
         "http://localhost:5173",    
-        "https://nano-url-app.vercel.app"   
+        "https://nano-url-three.vercel.app/"   
     ],
     credentials: true
 }));
